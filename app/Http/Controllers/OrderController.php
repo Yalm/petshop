dd0fa4a40fe1fd846e28a831a39d23d776572dd9
Yalm <renzomanuelc@gmail.com>
2019-08-26 23:53:20 -0500

    Shipping

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -43,7 +43,7 @@ class OrderController extends Controller
 
     public function show($id)
     {
-        $order = Order::with(['payment', 'products', 'state','customer'])->findOrFail($id);
+        $order = Order::with(['payment', 'products', 'state','customer','shipping'])->findOrFail($id);
 
         if (Auth::guard('user')->check()) {
             return response()->json($order);
